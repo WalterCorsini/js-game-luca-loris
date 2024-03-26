467c72682c9c26d06b8613c6f0598bb6101e903f
WalterCorsini <corsini.walter@gmail.com>
2024-03-26 19:30:05 +0100

    through the walls

--- js/script.js
+++ js/script.js
@@ -9,25 +9,26 @@ document.addEventListener("keydown", function(e) {
 
     if(e.code === "ArrowDown" && square.style.top != "280px"){
         square.style.top = `${square.offsetTop + 20}px`;
-        console.log(square.style.top, "top")
-    } else{
-        console.log("sei arrivato alla fine");
+    } else if(e.code === "ArrowDown" && square.style.top === "280px"){
+        square.style.top = 0 + "px";
     }
 
-// 
     if(e.code === "ArrowUp" && square.style.top > "0px"){
         square.style.top = `${square.offsetTop - 20}px`;
-        console.log(square.style.top, "top")
+    } else if(e.code === "ArrowUp" && square.style.top === "0px"){
+        square.style.top = 280 + "px";
     }
 
     if(e.code === "ArrowRight" && square.style.left != "280px"){
         square.style.left = square.offsetLeft + 20 + "px";
-        console.log(square.style.left, "left")
+    } else if(e.code === "ArrowRight" && square.style.left === "280px"){
+        square.style.left = 0 + "px";
     }
 
     if(e.code === "ArrowLeft" && square.style.left > "0px"){
         square.style.left = square.offsetLeft - 20 + "px";
-        console.log(square.style.left, "left")
+    } else if(e.code === "ArrowLeft" && square.style.left === "0px"){
+        square.style.left = 280 + "px";
     }
 
 });
