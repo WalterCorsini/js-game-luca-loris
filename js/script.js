c4e0dc6bc85cca2f316f9ee8fa833735d2469c88
WalterCorsini <corsini.walter@gmail.com>
2024-04-03 16:20:03 +0200

    add image pacman, block and rotate image

--- js/script.js
+++ js/script.js
@@ -26,36 +26,43 @@ console.log(blocksCoords[0]);
   }
 
 document.addEventListener("keydown", function (e) {
-
   // arrow down
   if (e.code === "ArrowDown" && square.style.top != "280px") {
+    square.style.transform = "rotate(90deg)";
     square.style.top = `${square.offsetTop + 20}px`;
     // arrow down end of run
   } else if (e.code === "ArrowDown" && square.style.top === "280px") {
+    square.style.transform = "rotate(90deg)";
     square.style.top = 0 + "px";
   }
 
   // arrow up end of run
   if (e.code === "ArrowUp" && square.style.top > "0px") {
+    square.style.transform = "rotate(-90deg)";
     square.style.top = `${square.offsetTop - 20}px`;
     // arrow up end of run
   } else if (e.code === "ArrowUp" && square.style.top === "0px") {
+    square.style.transform = "rotate(-90deg)";
     square.style.top = 280 + "px";
   }
 
-  // arrow rightn
+  // arrow right
   if (e.code === "ArrowRight" && square.style.left != "280px") {
+    square.style.transform = "rotate(0deg)";
     square.style.left = square.offsetLeft + 20 + "px";
     // arrow right end of run
   } else if (e.code === "ArrowRight" && square.style.left === "280px") {
+    square.style.transform = "rotate(0deg)";
     square.style.left = 0 + "px";
   }
 
   // arrow left
   if (e.code === "ArrowLeft" && square.style.left > "0px") {
+    square.style.transform = "rotate(180deg)";
     square.style.left = square.offsetLeft - 20 + "px";
     // arrow left end of run
   } else if (e.code === "ArrowLeft" && square.style.left === "0px") {
+    square.style.transform = "rotate(180deg)";
     square.style.left = 280 + "px";
   }
   // Collisioni
